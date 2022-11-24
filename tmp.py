2dea4c8b58a6f2588f5e1306c7cd3a8e4ce9ae6e
homelicat <vova.mischenko@icloud.com>
2022-11-24 21:02:20 +0100

    v2

--- tmp.py
+++ tmp.py
@@ -6,18 +6,18 @@ with open("test.tmp") as f:
 		src.extend(line.split())
 with open("test.s","w") as f:
 	ifs = 0
-	f.write("section .text\nglobal _start\n_start:\n")
+	func = False
+	funcs = list()
+	f.write("section .text\nglobal _start\n")
 	for com in src:
 		if com.isdecimal():
 			f.write("\tpush "+com+"\n")
 		elif com=="+":
 			f.write("\tpop eax\n\tpop ebx\n\tadd eax,ebx\n\tpush eax\n")
-		elif com=="exit":
-			f.write("\tmov eax,1\n\tpop ebx\n\tint 80h\n")
-		elif com=="dup":
-			f.write("\tpop eax\n\tpush eax\n\tpush eax\n")
 		elif com=="-":
 			f.write("\tpop eax\n\tpop ebx\n\tsub ebx,eax\n\tpush ebx\n")
+		elif com=="dup":
+			f.write("\tpop eax\n\tpush eax\n\tpush eax\n")
 		elif com=="drop":
 			f.write("\tpop eax\n")
 		elif com=="swap":
@@ -27,21 +27,43 @@ with open("test.s","w") as f:
 		elif com=="rot":
 			f.write("\tpop eax\n\tpop ebx\n\tpop ecx\n\tpush ebx\n\tpush eax\n\tpush ecx\n")
 		elif com=="=":
-			f.write("\tmov ecx,0\n\tmov edx,-1\n\tpop eax\n\tpop ebx\n\tcmp eax,ebx\n\tcmove ecx,edx\n\tpush ecx\n")
+			f.write("\txor ecx,ecx\n\tmov edx,-1\n\tpop eax\n\tpop ebx\n\tcmp eax,ebx\n\tcmove ecx,edx\n\tpush ecx\n")
 		elif com==">":
-			f.write("\tmov ecx,0\n\tmov edx,-1\n\tpop eax\n\tpop ebx\n\tcmp ebx,eax\n\tcmovg ecx,edx\n\tpush ecx\n")
+			f.write("\txor ecx,ecx\n\tmov edx,-1\n\tpop eax\n\tpop ebx\n\tcmp ebx,eax\n\tcmovg ecx,edx\n\tpush ecx\n")
 		elif com=="<":
-			f.write("\tmov ecx,0\n\tmov edx,-1\n\tpop eax\n\tpop ebx\n\tcmp ebx,eax\n\tcmovl ecx,edx\n\tpush ecx\n")
+			f.write("\txor ecx,ecx\n\tmov edx,-1\n\tpop eax\n\tpop ebx\n\tcmp ebx,eax\n\tcmovl ecx,edx\n\tpush ecx\n")
 		elif com=="and":
-			f.write("\tmov ecx,-1\n\tpop eax\n\tpop ebx\n\tcmp eax,0\n\tcmovne eax,ecx\n\tcmp ebx,0\n\tcmovne ebx,ecx\n\tand eax,ebx\n\tpush eax\n")
+			f.write("\tpop eax\n\tpop ebx\n\tand eax,ebx\n\tpush eax\n")
 		elif com=="or":
-			f.write("\tmov ecx,-1\n\tpop eax\n\tpop ebx\n\tcmp eax,0\n\tcmovne eax,ecx\n\tcmp ebx,0\n\tcmovne ebx,ecx\n\tor eax,ebx\n\tpush eax\n")
+			f.write("\tpop eax\n\tpop ebx\n\tor eax,ebx\n\tpush eax\n")
 		elif com=="invert":
-			f.write("\tpop eax\n\tmov ebx,-1\n\tcmp eax,0\n\tcmovne eax,ebx\n\tnot eax\n\tpush eax\n")
+			f.write("\tpop eax\n\tnot eax\n\tpush eax\n")
 		elif com=="if":
 			f.write("\tpop eax\n\tcmp eax,0\n\tje if"+str(ifs)+"\n")
 		elif com=="then":
 			f.write("if"+str(ifs)+":\n")
 			ifs+=1
+		elif com=="else":
+			f.write("\tjmp if"+str(ifs+1)+"\nif"+str(ifs)+":\n")
+			ifs+=1
+		elif com==":":
+			if func:
+				print("Error: already defining")
+				break
+			func=True
+		elif com==";":
+			if not func:
+				print("Error: not defining")
+				break
+			func=False
+			f.write("\tret\n")
+		elif com=="exit":
+			f.write("\tmov eax,1\n\tpop ebx\n\tint 80h\n")
 		else:
-			print("Error: "+com)
+			if com in funcs:
+				f.write("\tcall "+com+"\n")
+			elif func:
+				funcs.append(com)
+				f.write(com+":\n")
+			else:
+				print("Error: "+com)
